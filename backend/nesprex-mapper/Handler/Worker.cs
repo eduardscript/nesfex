798373df87003dbf6e0bc44ff6d5967e50389890
Eduardo Carvalho <eduardscript@protonmail.com>
2023-11-18 05:02:17 +0000

    Fixed project names

--- backend/nesprex-mapper/Handler/Worker.cs
+++ backend/nesprex-mapper/Handler/Worker.cs
@@ -22,35 +22,35 @@ public class Worker : BackgroundService
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        _logger.LogTrace("Nesprex Mapper started at: {time}", DateTimeOffset.Now);
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Nesprex Mapper running at: {time}", DateTimeOffset.Now);
-
             using var scope = _serviceProvider.CreateScope();
             var dataProvider = scope.ServiceProvider.GetRequiredService<IDataProvider>();
-            
-            var imagesData = await dataProvider
-                .GetImagesData();
-            
-            var technologies = await dataProvider
-                .GetTechnologies();
 
-            var imagesDataMessageBags = imagesData.Select(imageData => new MessageBag<ImageData>(
-                "images",
-                imageData.FileName, 
-                imageData));
-            
-            var technologiesMessageBags = technologies.Select(technology => new MessageBag<Technology>(
-                "technologies",
-                technology.Name, 
-                technology));
+            var imagesData = await dataProvider.GetImagesData();
+
+            var technologies = await dataProvider.GetTechnologies();
+
+            var imagesDataMessageBags = imagesData
+                .Select(imageData => new MessageBag<ImageData>(
+                    "images",
+                    imageData.FileName,
+                    imageData));
+
+            var technologiesMessageBags = technologies
+                .Select(technology => new MessageBag<Technology>(
+                    "technologies",
+                    technology.Name,
+                    technology));
 
             var tasks = new List<Task>
             {
                 _kafkaService.Produce(imagesDataMessageBags),
                 _kafkaService.Produce(technologiesMessageBags)
             };
-            
+
             await Task.WhenAll(tasks);
         }
     }
